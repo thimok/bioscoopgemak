aa615d9c20bd89230456d22b716b449d53a78860
Thimo Laptop <tkoolen1@student.avans.nl>
2017-03-31 09:26:58 +0200

    Finished settings layout

--- app/src/main/java/informatica/groep1/bioscoopapp/presentation/SettingsActivity.java
+++ app/src/main/java/informatica/groep1/bioscoopapp/presentation/SettingsActivity.java
@@ -5,11 +5,25 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import informatica.groep1.bioscoopapp.R;
 
 public class SettingsActivity extends MenuActivity {
+	
+	//================================================================================
+	// Properties
+	//================================================================================
+	
+	private TextView languageSelected;
+	private TextView languageChoose;
+	private ImageView languageIcon;
+	
+	private TextView accountText;
+	private ImageView accountIcon;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +34,53 @@ public class SettingsActivity extends MenuActivity {
 		getSupportActionBar().setDisplayShowTitleEnabled(true);
 		getSupportActionBar().setTitle("Settings");
 		super.onCreateDrawer(toolbar, this);
+		
+		languageSelected = (TextView) findViewById(R.id.settingsActivity_TV_languageSelected);
+		languageSelected.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				changeLanguage();
+			}
+		});
+		
+		languageChoose = (TextView) findViewById(R.id.settingsActivity_TV_languageText);
+		languageChoose.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				changeLanguage();
+			}
+		});
+		
+		languageIcon = (ImageView) findViewById(R.id.settingsActivity_IV_languageIcon);
+		languageIcon.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				changeLanguage();
+			}
+		});
+		
+		accountText = (TextView) findViewById(R.id.settingsActivity_TV_accountText);
+		accountText.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				changeAccount();
+			}
+		});
+		
+		accountIcon = (ImageView) findViewById(R.id.settingsActivity_IV_accountIcon);
+		accountIcon.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				changeAccount();
+			}
+		});
 	}
 	
+	private void changeLanguage() {
+		Log.i("Settings", "Language selected");
+	}
+	
+	private void changeAccount() {
+		Log.i("Settings", "Account selected");
+	}
 }
