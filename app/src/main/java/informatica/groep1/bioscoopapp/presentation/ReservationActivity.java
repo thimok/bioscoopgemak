219c0c6471d043cdfe18af7a0974a3d409ac5ec4
Sven Westerlaken <sven.westerlaken@hotmail.nl>
2017-04-04 22:06:06 +0200

    Added intent for reservation screen

--- app/src/main/java/informatica/groep1/bioscoopapp/presentation/ReservationActivity.java
+++ app/src/main/java/informatica/groep1/bioscoopapp/presentation/ReservationActivity.java
@@ -11,10 +11,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import informatica.groep1.bioscoopapp.R;
+import informatica.groep1.bioscoopapp.domain.Screening;
 
 public class ReservationActivity extends AppCompatActivity {
+
+    private Screening screening;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,16 @@ public class ReservationActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        screening = (Screening) getIntent().getSerializableExtra("Screening");
+
+        TextView title = (TextView) findViewById(R.id.reservationActivity_TV_movieTitle);
+        TextView time = (TextView) findViewById(R.id.reservationActivity_TV_time);
+        TextView date = (TextView) findViewById(R.id.reservationActivity_TV_date);
+
+        title.setText(screening.getTitle());
+        time.setText(screening.getStartTime());
+        date.setText(screening.getPlayDate());
     }
 
 }
--- app/src/main/java/informatica/groep1/bioscoopapp/presentation/ShowDetailedActivity.java
+++ app/src/main/java/informatica/groep1/bioscoopapp/presentation/ShowDetailedActivity.java
@@ -43,8 +43,9 @@ public class ShowDetailedActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(getApplicationContext(), ReservationActivity.class);
+                i.putExtra("Screening", screening);
+                startActivity(i);
             }
         });
 
