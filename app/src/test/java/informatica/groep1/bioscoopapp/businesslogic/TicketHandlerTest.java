6d9865dabb173d8bd6be35413110efda63d8ca1e
Twanvm <twan.vm@hotmail.com>
2017-04-05 15:07:51 +0200

    commit voor het mergen van Sven's aanpassingen aan TicketHandler

---
+++ app/src/test/java/informatica/groep1/bioscoopapp/businesslogic/TicketHandlerTest.java
@@ -0,0 +1,36 @@
+package informatica.groep1.bioscoopapp.businesslogic;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by twanv on 5-4-2017.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class TicketHandlerTest {
+
+    TicketHandler ticketHandler;
+
+    @Before
+    public void setUp() throws Exception {
+        ticketHandler = new TicketHandler();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getPrice() throws Exception {
+        Assert.asserE
+    }
+
+}
